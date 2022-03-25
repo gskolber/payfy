fbe7b53977fb9c4f26e749d0d21e72c683d5eb54
Gabriel Kolber <gabrielkolber@icloud.com>
2022-03-24 23:53:18 -0300

    dockerizing the application

--- src/config/dev.exs
+++ src/config/dev.exs
@@ -5,7 +5,7 @@ config :payfy, Payfy.Repo,
   username: System.get_env("DB_USER") || "postgres",
   password: System.get_env("DB_PASS") || "postgres",
   database: "payfy_dev",
-  hostname: "localhost",
+  hostname: "db",
   log: false,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -19,7 +19,7 @@ config :payfy, Payfy.Repo,
 config :payfy, PayfyWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
